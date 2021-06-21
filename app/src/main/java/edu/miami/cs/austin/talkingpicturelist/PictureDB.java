84fc77f4138b4406909aa1e08a3e1cdeeaf73b40
QuiGonGirth <austinSchladant@gmail.com>
2021-06-21 18:31:13 -0400

    Initial commit

---
+++ app/src/main/java/edu/miami/cs/austin/talkingpicturelist/PictureDB.java
@@ -0,0 +1,10 @@
+package edu.miami.cs.austin.talkingpicturelist;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {PictureEntity.class}, version = 1, exportSchema = false)
+public abstract class PictureDB extends RoomDatabase {
+
+    public abstract PictureAccess daoAccess();
+}
